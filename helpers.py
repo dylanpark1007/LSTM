4f4dc38bc2e48f514a82053f36cc69046981fc82
dylanpark1007 <63712036+dylanpark1007@users.noreply.github.com>
2020-04-15 19:17:50 +0900

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -29,15 +29,12 @@ def time_since(since, percent):
     return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
 
 
-# Lowercase, trim, and remove non-letter characters
 def normalize_string(s):
     s = unicode_to_ascii(s.lower().strip())
     s = re.sub(r"([.!?])", r" \1", s)
     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
     return s
 
-
-# Turns a unicode string to plain ASCII (http://stackoverflow.com/a/518232/2809427)
 def unicode_to_ascii(s):
     chars = [c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn']
     char_list = ''.join(chars)
