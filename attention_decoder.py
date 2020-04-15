3ea08eed8da742edf9d09252eac0061e65a1fdf9
dylanpark1007 <63712036+dylanpark1007@users.noreply.github.com>
2020-04-15 19:13:43 +0900

    Update attention_decoder.py

--- attention_decoder.py
+++ attention_decoder.py
@@ -6,7 +6,6 @@ from attention import Attention
 
 
 class AttentionDecoderRNN(nn.Module):
-    """Recurrent neural network that makes use of gated recurrent units to translate encoded inputs using attention."""
 
     def __init__(self, attention_model, hidden_size, output_size, n_layers=1, dropout_p=.1):
         super(AttentionDecoderRNN, self).__init__()
@@ -21,30 +20,11 @@ class AttentionDecoderRNN(nn.Module):
         self.gru = nn.GRU(hidden_size * 2, hidden_size, n_layers, dropout=dropout_p)
         self.out = nn.Linear(hidden_size * 2, output_size)
 
-        # Choose attention model
+        # Choose attention model (ToDo)
         if attention_model is not None:
             self.attention = Attention(attention_model, hidden_size)
 
     def forward(self, word_input, last_context, last_hidden, encoder_outputs):
-        """Run forward propagation one step at a time.
-        
-        Get the embedding of the current input word (last output word) [s = 1 x batch_size x seq_len]
-        then combine them with the previous context. Use this as input and run through the RNN. Next,
-        calculate the attention from the current RNN state and all encoder outputs. The final output
-        is the next word prediction using the RNN hidden state and context vector.
-        
-        Args:
-            word_input: torch Variable representing the word input constituent
-            last_context: torch Variable representing the previous context
-            last_hidden: torch Variable representing the previous hidden state output
-            encoder_outputs: torch Variable containing the encoder output values
-            
-        Return:
-            output: torch Variable representing the predicted word constituent 
-            context: torch Variable representing the context value
-            hidden: torch Variable representing the hidden state of the RNN
-            attention_weights: torch Variable retrieved from the attention model
-        """
 
         # Run through RNN
         word_embedded = self.embedding(word_input).view(1, 1, -1)
